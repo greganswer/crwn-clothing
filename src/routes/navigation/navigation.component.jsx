ed3434b719031315b7ff239b8a40237b20a239f0
Greg Answer <greganswer@users.noreply.github.com>
2023-10-03 23:25:26 -0400

    Style and finish Navigation component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -7,10 +7,22 @@ import './navigation.styles.scss';
 
 const Navigation = () => {
   return (
-    <div>
-    <h1>Navigation</h1>
-    <Outlet />
-    </div>
+    <Fragment>
+      <div className='navigation'>
+        <Link className='logo-container' to='/'>
+          <CrwnLogo className='logo' />
+        </Link>
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>
+            SHOP
+          </Link>
+          <Link className='nav-link' to='/sign-in'>
+            SIGN IN
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
   );
 };
 
